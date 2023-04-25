cc36866956f63095388d3819a603b629ac208805
Omid <39193273+omidkheiri@users.noreply.github.com>
2023-04-25 14:59:55 +0300

    Add project files.

---
+++ Umbraco.Core/PropertyEditors/PropertyValueConverterBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Core.PropertyEditors
+{
+    /// <summary>
+    /// Provides a default overridable implementation for <see cref="IPropertyValueConverter"/> that does nothing.
+    /// </summary>
+    public abstract class PropertyValueConverterBase : IPropertyValueConverter
+    {
+        public virtual bool IsConverter(IPublishedPropertyType propertyType)
+            => false;
+
+        public virtual bool? IsValue(object value, PropertyValueLevel level)
+        {
+            switch (level)
+            {
+                case PropertyValueLevel.Source:
+                    return value != null && (!(value is string) || string.IsNullOrWhiteSpace((string) value) == false);
+                default:
+                    throw new NotSupportedException($"Invalid level: {level}.");
+            }
+        }
+
+        public virtual bool HasValue(IPublishedProperty property, string culture, string segment)
+        {
+            // the default implementation uses the old magic null & string comparisons,
+            // other implementations may be more clever, and/or test the final converted object values
+            var value = property.GetSourceValue(culture, segment);
+            return value != null && (!(value is string) || string.IsNullOrWhiteSpace((string) value) == false);
+        }
+
+        /// <inheritdoc />
+        public virtual Type GetPropertyValueType(IPublishedPropertyType propertyType)
+            => typeof (object);
+
+        /// <inheritdoc />
+        public virtual PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType)
+            => PropertyCacheLevel.Snapshot;
+
+        /// <inheritdoc />
+        public virtual object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
+            => source;
+
+        /// <inheritdoc />
+        public virtual object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+            => inter;
+
+        /// <inheritdoc />
+        public virtual object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+            => inter?.ToString() ?? string.Empty;
+    }
+}
